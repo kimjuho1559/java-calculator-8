3661d041a6834e444172efba8f6e6af6526eb70a
jhkim <juho1589@gmail.com>
2025-10-17 02:12:48 +0900

    <feat>($location): create new view package
    New view package include inputView outputView

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,14 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    // 입력 받는 함수
+    public String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
+
+
