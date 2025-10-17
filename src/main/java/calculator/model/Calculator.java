4507c2fb599d052e14333f2c99ad9f0e17f954ed
jhkim <juho1589@gmail.com>
2025-10-17 16:17:55 +0900

    <feat>(code): update calculator
    - Receive string input and return its value calculation

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,7 +1,17 @@
 package calculator.model;
 
 public class Calculator {
+    private final String value;
+    public Calculator(String value) {
+        this.value = value;
+    }
     public int calculator() {
-        return 0;
+        Number number = new Number();
+        int [] num = number.separateNum(value);
+        int total = 0;
+        for (int i = 0; i < num.length; i++) {
+            total += num[i];
+        }
+        return total;
     }
 }
