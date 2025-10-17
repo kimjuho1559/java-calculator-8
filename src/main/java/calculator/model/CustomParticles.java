de36bea85d0557f8b81c9d7d584b4979dc1dc233
jhkim <juho1589@gmail.com>
2025-10-17 14:53:41 +0900

    <feat>(feature): create new feature CustomParticles
    - Function to separate custom separator
    - Invalid input value exception handling

--- src/main/java/calculator/model/CustomParticles.java
+++ src/main/java/calculator/model/CustomParticles.java
@@ -1,4 +1,31 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomParticles {
+    public List<String> separateParticles(String value) {
+        String [] values = value.split("");
+        List<String> list = new ArrayList<>();
+        list.add(",");
+        list.add(":");
+        if (values[0].equals("/") && values[1].equals("/")) {
+            for (int i = 2; i < values.length; i++) {
+                if (i == values.length - 1) {
+                    invalidCustomParticles();
+                }
+                if (values[i].equals("\n")) {
+                    break;
+                }
+                list.add(values[i]);
+            }
+            return list;
+        } else if (values[0].equals("/") || values[1].equals("/")) {
+            invalidCustomParticles();
+        }
+        return list;
+    }
+    private void invalidCustomParticles() {
+        throw new IllegalArgumentException("올바른 커스텀 생성자를 사용해주세요");
+    }
 }
